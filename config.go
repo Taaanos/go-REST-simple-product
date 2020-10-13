26155eb93b9fe2fa3fc0f70cfe8b2a439cdf4d40
Thanos Amoutzias <ktmfuji@gmail.com>
2020-10-13 15:24:58 +0100

    Added postgres configuration to be loaded w a .config

---
+++ config.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type PostgresConfig struct {
+	Host     string `json: "host"`
+	Port     int    `json: "port"`
+	User     string `json: "user"`
+	Password string `json: "password"`
+	Name     string `json: "name"`
+}
+
+func (c PostgresConfig) ConnectionInfo() string {
+	if c.Password == "" {
+		return fmt.Sprintf("host=%s port=%d user=%s  dbname=%s sslmode=disable",
+			c.Host, c.Port, c.User, c.Name)
+	}
+	return fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		c.Host, c.Port, c.User, c.Password, c.Name)
+}
+
+func DefaultPostgresConfig() PostgresConfig {
+	return PostgresConfig{
+		Host:     "localhost",
+		Port:     5432,
+		User:     "tanos",
+		Password: "password",
+		Name:     "products_demo",
+	}
+}
+
+type Config struct {
+	Port     int            `json:"port"`
+	Env      string         `json:"env"`
+	Database PostgresConfig `json:"database"`
+}
+
+func (c Config) IsProd() bool {
+	return c.Env == "prod"
+}
+
+func DefaultConfig() Config {
+	return Config{
+		Port:     3000,
+		Env:      "dev",
+		Database: DefaultPostgresConfig(),
+	}
+}
+
+func LoadConfig(configReq bool) Config {
+	f, err := os.Open(".config")
+	if err != nil {
+		if configReq {
+			panic(err)
+		}
+		fmt.Println("Using the default config...")
+		return DefaultConfig()
+	}
+	var c Config
+	dec := json.NewDecoder(f)
+	err = dec.Decode(&c)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Successfully loaded .config")
+	return c
+}
